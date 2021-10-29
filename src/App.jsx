3454146e8f0bec32c573c7b8929cbfc31a53d1a8
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-29 23:27:00 +0900

    TODOリストをuseStateで実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // Todoリストのコンポーネント化
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaaa", "iiiiiii"]);
+  const [completeTodos, setCompleteTodos] = useState(["uuuuuuu", "eeeeeee"]);
   return (
     <>
       <div className="input-area">
@@ -9,19 +13,30 @@ export const App = () => {
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
-        <div className="list-row">
-          <li>aaaaaaa</li>
-          <button>完了</button>
-          <button>削除</button>
-        </div>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
-        <div className="list-row">
-          <li>aaaaaaa</li>
-          <button>完了</button>
-          <button>削除</button>
-        </div>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
